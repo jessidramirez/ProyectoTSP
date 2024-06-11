acd103180b32343fe3c86b419865d48379cb316d
Jessid Ramirez <jessid.alexander@gmail.com>
2024-06-11 17:24:34 -0500

    update

--- database/db.js
+++ database/db.js
@@ -4,7 +4,6 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
-    
 });
 
 connection.connect((error)=>{
